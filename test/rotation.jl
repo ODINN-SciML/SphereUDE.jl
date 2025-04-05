de883f08835b7c1935508c3e4a2143c08b10088f
Facundo Sapienza <sapienza@stanford.edu>
2025-04-05 01:22:35 -0700

    Update test/rotation.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/rotation.jl
+++ test/rotation.jl
@@ -50,12 +50,7 @@ function test_single_rotation(; repeat_times = false)
             λ = 0.1,
             diff_mode = FiniteDifferences(1e-6),
         ),
-        Regularization(
-            order = 0,
-            power = 2.0,
-            λ = 0.001,
-            diff_mode = nothing,
-        ),
+        Regularization(order = 0, power = 2.0, λ = 0.001, diff_mode = nothing),
     ]
 
     params = SphereParameters(
